93fbc6bacae6a64b2870427db2f41ee6babde53a
robert <robertndive29@gmail.com>
2024-04-30 14:22:56 +0100

    initial commit

---
+++ pythontutorial/pythontutorial/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.urls import path,include
+
+urlpatterns = [
+       path('',include('Authentication.urls')),
+       path('admin/', admin.site.urls)
+ 
+]
